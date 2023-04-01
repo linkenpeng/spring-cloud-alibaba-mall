4abbc3a68a9ea33b5a202adb97772cd83dcae879
linken <collin_linken@qq.com>
2023-04-01 16:53:03 +0800

    add nuxt hello

--- ui-front/layouts/default.vue
+++ ui-front/layouts/default.vue
@@ -1,53 +1,36 @@
 <template>
-  <div>
-    <nuxt/>
+  <div id="app-container">
+    <div id="main">
+
+      <header/>
+
+      <div class="main-container">
+        <el-scrollbar class="page-component__scroll">
+
+          <nuxt/>
+
+        </el-scrollbar>
+      </div>
+
+      <footer/>
+
+    </div>
+
   </div>
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+import '~/assets/css/app.css'
+import '~/assets/css/chunk.css'
+import '~/assets/css/iconfont.css'
+import '~/assets/css/main.css'
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
+import header from './header'
+import footer from './footer'
+export default {
+  components: {
+    header, footer
+  }
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
 
---
+++ ui-front/layouts/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header-container">
+    <div class="wrapper">
+        <div>
+          <span>intecsec mall</span>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import '~/assets/css/app.css'
+import '~/assets/css/chunk.css'
+import '~/assets/css/iconfont.css'
+import '~/assets/css/main.css'
+export default {
+
+}
+</script>
+
